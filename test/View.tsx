87bb4a238d16a7e50a5b4ccfe2e97d1ebee3b215
gs-kgopi <kgopi@gainsight.com>
2018-09-11 15:10:09 +0530

    Updated view

--- test/View.tsx
+++ test/View.tsx
@@ -4,14 +4,14 @@ export default class View{
 
     constructor(state, actions){
         return (<div a="2" b="4">
-            <div>It's Working</div>
+            <h1>G DOM playground</h1>
             <div onclick={function(){
                 actions.updateList(Math.floor(Math.random()*1000));
             }}>Click to add</div>
             <div>
                 <ul>
                     {
-                        state.list.map(item=><li onclick={function(){actions.removeItem(+this.innerText)}} >{item+""}</li>)
+                        state.list.map(item=><li onclick={function(){actions.removeItem(+this.innerText)}} >{item+""}<span style="margin-left: 20px">Click to remove</span></li>)
                     }
                 </ul>
             </div>
